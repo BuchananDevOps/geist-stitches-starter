ff323e1092b6c0d4f6db0dcab79d057e333d0129
John Buchanan <john@buchanandevops.com>
2023-03-05 13:03:45 -0500

    geist-ui with stitches and complete eslint, prettier, jest testing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+const runtimeCaching = require("next-pwa/cache")
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  runtimeCaching,
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    API_KEY: process.env.API_KEY,
+    HOST: process.env.HOST,
+  },
   reactStrictMode: true,
+  swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = () => {
+  const plugins = [withBundleAnalyzer, withPWA]
+  return plugins.reduce((acc, plugin) => plugin(acc), {
+    ...nextConfig,
+  })
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,43 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import NextDocument, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document"
+import { Fragment } from "react"
+import React from "react"
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+import { CssBaseline } from "@geist-ui/core"
+import { createStitches } from "@stitches/react"
+
+class Document extends NextDocument {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await NextDocument.getInitialProps(ctx)
+    const styles = CssBaseline.flush()
+    return {
+      ...initialProps,
+      styles: [
+        <Fragment key="1">
+          {initialProps.styles}
+          {styles}
+        </Fragment>,
+      ],
+    }
+  }
+
+  render() {
+    return (
+      <Html itemType="https://schema.org/ProfessionalService" lang="en">
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export const { getCssText } = createStitches()
+export default Document
